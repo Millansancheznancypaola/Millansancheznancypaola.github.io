9f3f0aa7e1786c1091f6a3162981e4bbced99058
Millán Sánchez Nancy Paola <159413815+Millansancheznancypaola@users.noreply.github.com>
2024-06-01 13:16:39 -0700

    spotify.sql avance

--- Parcial 3/spotify.sql
+++ Parcial 3/spotify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2024 a las 07:02:24
+-- Tiempo de generación: 01-06-2024 a las 22:15:18
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -44,9 +44,9 @@ INSERT INTO `artistas` (`id`, `nombre`, `descripción`) VALUES
 (4, 'Daft Punk', 'As they evolved from \'90s French house pioneers to 2000s dance tastemakers to mainstream heroes in the 2010s, Daft Punk remained one of dance music\'s most iconic acts. With their early singles and 1997\'s instant-classic debut album Homework, Guy-Manuel de Homem-Christo and  quickly won acclaim for t'),
 (5, 'The Weeknd', 'The Weeknd took over pop music & culture on his own terms filtering R&B, Pop,& hip-hop through an ambitious widescreen lens. The multi-platinum 3X GRAMMY Award winner has emerged as one of the most successful & significant artists of the modern era.'),
 (6, 'MEGADETH', 'Released by UMe on September 2, 2022, The Sick, The Dying... And The Dead! further establishes MEGADETH as a band that has both defined and repeatedly redefined heavy metal since its formation. The album follows up 2016’s Grammy®-winning Dystopia, which debuted at #3 on the Billboard Top 200 (MEGADE'),
-(7, 'Iron Maiden', 'Iron Maiden are an institution. Over the course of 44 years they have come to embody a spirit of fearless creative independence, ferocious dedication to their fans, and a cheerful indifference to their critics that’s won them a following that spans every culture, generation, and time-zone. A story o'),
+(7, 'Gorillaz', 'Created by musician Damon Albarn and artist Jamie Hewlett, Gorillaz is singer 2D, bassist Murdoc Niccals, drummer Russel Hobbs and guitarist Noodle. The BRIT and Grammy-winning virtual band formed following a collision of mishaps, meetings and pure luck to blow up a pre-digital world with their colo'),
 (8, 'BLACKPINK', 'BLACKPINK, which took over the hearts of listeners around the world with their mega-hit songs such as ‘DDU-DU DDU-DU’, ‘Kill This Love’ and ‘How You Like That’ has made a comeback after a year and 10 months.'),
-(9, 'ABBA', 'ABBA, whose name is made up of the initials of the four members’ first names – Agnetha Fältskog, Björn Ulvaeus, Benny Andersson and Anni-Frid “Frida” Lyngstad – was one of the biggest pop groups of the 1970s. Forming in 1972, the Swedish band burst onto the world stage two years later with their meg'),
+(9, 'Zoe', 'A Grammy- and Latin Grammy-winning five-piece band from Mexico with a hook-laden, psychedelic rock style and infectious rock and pop songs, Zoé are regarded as one of the most important groups of the indie rock era. The band rose to the top of the rock en español scene during the late 2000s, and the'),
 (10, 'Soda Stereo', 'Soda Stereo were one of the most important Argentine pop/rock bands of the \'80s and early \'90s. Created as a trio in 1982, the band was formed by guitarist/vocalist , bass player Zeta Bosio (born Héctor Bosio), and drummer Charly Alberti (born Carlos Ficcichia). Their influence spread to the rest of');
 
 -- --------------------------------------------------------
@@ -164,7 +164,58 @@ INSERT INTO `artistas_canciones` (`id`, `artista_id`, `cancion_id`) VALUES
 (96, 5, 96),
 (97, 5, 97),
 (98, 5, 98),
-(99, 5, 99);
+(99, 5, 99),
+(100, 6, 100),
+(101, 6, 101),
+(102, 6, 102),
+(103, 6, 103),
+(104, 6, 104),
+(105, 6, 105),
+(106, 6, 106),
+(107, 6, 107),
+(108, 6, 108),
+(109, 6, 109),
+(110, 6, 110),
+(111, 6, 111),
+(112, 6, 112),
+(113, 6, 113),
+(114, 6, 114),
+(115, 6, 115),
+(116, 6, 116),
+(117, 6, 117),
+(118, 7, 118),
+(119, 7, 119),
+(120, 7, 120),
+(121, 7, 121),
+(122, 7, 122),
+(123, 7, 123),
+(124, 7, 124),
+(125, 7, 125),
+(126, 7, 126),
+(127, 7, 127),
+(128, 7, 128),
+(129, 7, 129),
+(130, 7, 130),
+(131, 7, 131),
+(132, 7, 132),
+(133, 7, 133),
+(134, 7, 134),
+(135, 7, 135),
+(136, 7, 136),
+(137, 8, 137),
+(138, 8, 138),
+(139, 8, 139),
+(140, 8, 140),
+(141, 8, 141),
+(142, 8, 142),
+(143, 8, 143),
+(144, 8, 144),
+(145, 8, 145),
+(146, 8, 146),
+(147, 8, 147),
+(148, 10, 148),
+(149, 10, 149),
+(150, 9, 150);
 
 -- --------------------------------------------------------
 
@@ -283,7 +334,58 @@ INSERT INTO `canciones` (`id`, `nombre`, `duracion`, `activo`, `genero_id`) VALU
 (96, 'How Do I Make You Love Me?', '00:03:34', 0, 4),
 (97, 'A Lonely Night', '00:03:40', 0, 4),
 (98, 'I Was Never There', '00:04:01', 0, 4),
-(99, 'Blinding Lights', '00:03:20', 0, 4);
+(99, 'Blinding Lights', '00:03:20', 0, 4),
+(100, 'Symphony Of Destruction', '00:04:06', 0, 5),
+(101, 'Holy Wars... The Punishment Due', '00:06:36', 0, 5),
+(102, 'Hangar 18', '00:05:14', 0, 5),
+(103, 'Take No Prisoners', '00:03:28', 0, 5),
+(104, 'À tout le monde', '00:04:22', 0, 5),
+(105, 'Lucretia', '00:03:58', 0, 5),
+(106, 'Tornado of Souls', '00:05:22', 0, 5),
+(107, 'Dawn Patrol', '00:01:50', 0, 5),
+(108, 'Rust in Peace', '00:05:36', 0, 5),
+(109, 'The Threat Is Real', '00:04:22', 0, 5),
+(110, 'Dystopia', '00:04:59', 0, 5),
+(111, 'Promises', '00:04:28', 0, 5),
+(112, 'The Conjuring', '00:05:02', 0, 5),
+(113, 'Peace Sells', '00:04:02', 0, 5),
+(114, 'Victory', '00:04:26', 0, 5),
+(115, 'Addicted to Chaos', '00:05:27', 0, 5),
+(116, 'The Killing Road', '00:03:51', 0, 5),
+(117, 'Youthanasia', '00:04:07', 0, 5),
+(118, 'Feel Good Inc. (Feel Good Inc)', '00:03:42', 0, 6),
+(119, 'On Melancholy Hill', '00:03:53', 0, 6),
+(120, 'DARE', '00:04:04', 0, 6),
+(121, 'Rhinestone Eyes', '00:03:20', 0, 6),
+(122, 'She\'s My Collar', '00:03:29', 0, 6),
+(123, 'Busted and Blue', '00:04:37', 0, 6),
+(124, 'Ticker Tape', '00:04:28', 0, 6),
+(125, 'Andromeda', '00:03:17', 0, 6),
+(126, 'Let Me Out', '00:02:55', 0, 6),
+(127, 'Saturnz Barz', '00:03:01', 0, 6),
+(128, 'Out of Body', '00:03:34', 0, 6),
+(129, 'The Apprentice', '00:03:54', 0, 6),
+(130, 'Carnival', '00:02:15', 0, 6),
+(131, 'Strobelite', '00:04:32', 0, 6),
+(132, 'Momentz', '00:03:16', 0, 6),
+(133, 'Kids With Guns', '00:03:45', 0, 6),
+(134, 'New Gold', '00:03:36', 0, 6),
+(135, 'Cracker Island', '00:03:36', 0, 6),
+(136, 'Rock the House', '00:03:03', 0, 6),
+(137, 'Boombayah', '00:04:00', 0, 7),
+(138, 'AS IF IT\'S YOUR LAST', '00:03:33', 0, 7),
+(139, 'Crazy Over You', '00:02:45', 0, 7),
+(140, 'DDU-DU DDU-DU', '00:03:31', 0, 7),
+(141, 'How You Like That', '00:03:02', 0, 7),
+(142, 'Lovesick Girls', '00:03:14', 0, 7),
+(143, 'Love to Hate Me', '00:02:51', 0, 7),
+(144, 'Pretty Savage', '00:03:21', 0, 7),
+(145, 'You Never Know', '00:03:51', 0, 7),
+(146, 'Bet You Wanna', '00:02:41', 0, 7),
+(147, 'Kill This Love', '00:03:11', 0, 7),
+(148, 'El ritmo de tus ojos', '00:03:57', 0, 8),
+(149, 'Planta', '00:04:02', 0, 8),
+(150, 'Vía láctea', '00:04:13', 0, 8);
 
 -- --------------------------------------------------------
 
@@ -317,7 +419,10 @@ INSERT INTO `generos` (`id`, `nombre`) VALUES
 (2, 'Balada'),
 (3, 'Electrónica'),
 (4, 'R&B/Soul'),
-(5, 'Metal');
+(5, 'Metal'),
+(6, 'Hip-hop/art pop'),
+(7, 'K-Pop'),
+(8, 'Rock');
 
 -- --------------------------------------------------------
 
@@ -386,7 +491,16 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `membresia_id`) VALUES
-(1, 'Nancy Millán', 'nancy.millan22@cetis107.edu.mx', 2);
+(1, 'Nancy Millán', 'nancy.millan22@cetis107.edu.mx', 2),
+(2, 'Juan Pérez', 'juan.perez@gmail.com', 2),
+(3, 'María González', 'maria.gonzalez@gmail.com', 1),
+(4, 'Carlos López', 'carlos.lopez@gmail.com', 3),
+(5, 'Ana Martínez', 'ana.martinez@gmail.com', 1),
+(6, 'Pedro García', 'pedro.garcia@gmail.com', 3),
+(7, 'Laura Fernández', 'laura.fernandez@gmail.com', 2),
+(8, 'Diego Ramírez', 'diego.ramirez@gmail.com', 3),
+(9, 'Elena Torres', 'elena.torres@gmail.com', 1),
+(10, 'Miguel Sánchez', 'miguel.sanchez@gmail.com', 2);
 
 --
 -- Índices para tablas volcadas
@@ -476,13 +590,13 @@ ALTER TABLE `artistas`
 -- AUTO_INCREMENT de la tabla `artistas_canciones`
 --
 ALTER TABLE `artistas_canciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=151;
 
 --
 -- AUTO_INCREMENT de la tabla `canciones`
 --
 ALTER TABLE `canciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=162;
 
 --
 -- AUTO_INCREMENT de la tabla `favoritas`
@@ -494,7 +608,7 @@ ALTER TABLE `favoritas`
 -- AUTO_INCREMENT de la tabla `generos`
 --
 ALTER TABLE `generos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `membresías`
@@ -518,7 +632,7 @@ ALTER TABLE `playlists_canciones`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
