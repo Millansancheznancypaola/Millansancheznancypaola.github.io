2eac7fbacb4c4e1001a54585e9a32ca5294ddec7
Millán Sánchez Nancy Paola <159413815+Millansancheznancypaola@users.noreply.github.com>
2024-08-30 18:49:17 -0700

    Práctica 1 30/08/2024
    Millán Sánchez Nancy Paola 5AVP 30/08/2024

---
+++ BasedeDatosWeb/parcial1/holamundo.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$nombre ="Nancy";
+$numero = 4;
+$numero2 ="4";
+
+$total = $numero + $numero2;
+
+echo "Total =".$total;
+echo"<h1> HOLA MUNDO </h1>";
+
+for($i=0;$i<10;$i++){
+    echo $i. "<br>";
+}
+
+echo"<h3 style='color:purple;'> Millán Sánchez Nancy paola</h3>";
+
+?>
