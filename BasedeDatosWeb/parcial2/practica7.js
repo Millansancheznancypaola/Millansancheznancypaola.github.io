984b37360e0106d87de9a60cec088cf8252a340f
Millán Sánchez Nancy Paola <159413815+Millansancheznancypaola@users.noreply.github.com>
2024-10-02 21:43:04 -0700

    practica #7 02/10/2024

--- BasedeDatosWeb/parcial2/practica7.js
+++ BasedeDatosWeb/parcial2/practica7.js
@@ -19,12 +19,121 @@ function validar(posicion){
             case 1:
                 if(cuadro1 == cuadro2 && cuadro1== cuadro3){
                     alert("GANADOR --->" + cuadro1)
+                    pintar(1,2,3);
                 } else if(cuadro1 == cuadro5 && cuadro1== cuadro9){
                     alert("GANADOR --->" + cuadro1)
+                    pintar(1,5,9);
                 } else if(cuadro1 == cuadro4 && cuadro1 == cuadro7){
                     alert("GANADOR --->" + cuadro1)
+                    pintar(1,4,7);
                 }
              break;
+
+
+             case 2:
+                if(cuadro2 == cuadro1 && cuadro2== cuadro3){
+                    alert("GANADOR --->" + cuadro2)
+                    pintar(1,2,3);
+                } else if(cuadro2 == cuadro5 && cuadro2== cuadro8){
+                    alert("GANADOR --->" + cuadro2)
+                    pintar(2,5,8);
+                } 
+
+             break;
+
+
+               case 3:
+                if (cuadro3 == cuadro2 && cuadro3 == cuadro1) {
+                    alert("GANADOR ---> " + cuadro3);
+                    pintar(3, 2, 1);
+                } else if (cuadro3 == cuadro5 && cuadro3 == cuadro7) {
+                    alert("GANADOR ---> " + cuadro3);
+                    pintar(3, 5, 7);
+                } else if (cuadro3 == cuadro6 && cuadro3 == cuadro9) {
+                    alert("GANADOR ---> " + cuadro1);
+                    pintar(3, 6, 9);
+                }
+                break;
+
+            case 4:
+                if (cuadro4 == cuadro1 && cuadro4 == cuadro7) {
+                    alert("GANADOR ---> " + cuadro4);
+                    pintar(1, 4, 7);
+                } else if (cuadro4 == cuadro5 && cuadro4 == cuadro6) {
+                    alert("GANADOR ---> " + cuadro4);
+                    pintar(4, 5, 6);
+                }
+                break;
+
+            case 5:
+                if (cuadro5 == cuadro1 && cuadro5 == cuadro9) {
+                    alert("GANADOR ---> " + cuadro5);
+                    pintar(1, 5, 9);
+                } else if (cuadro5 == cuadro2 && cuadro5 == cuadro8) {
+                    alert("GANADOR ---> " + cuadro5);
+                    pintar(2, 5, 8);
+                } else if (cuadro5 == cuadro3 && cuadro5 == cuadro7) {
+                    alert("GANADOR ---> " + cuadro5);
+                    pintar(3, 5, 7);
+                } else if (cuadro5 == cuadro4 && cuadro5 == cuadro6) {
+                    alert("GANADOR ---> " + cuadro5);
+                    pintar(4, 5, 6);
+                }
+                break;
+
+            case 6:
+                if (cuadro6 == cuadro4 && cuadro6 == cuadro5) {
+                    alert("GANADOR ---> " + cuadro6);
+                    pintar(4, 5, 6);
+                } else if (cuadro6 == cuadro3 && cuadro6 == cuadro9) {
+                    alert("GANADOR ---> " + cuadro6);
+                    pintar(3, 6, 9);
+                }
+                break;
+
+            case 7:
+                if (cuadro7 == cuadro1 && cuadro7 == cuadro4) {
+                    alert("GANADOR ---> " + cuadro7);
+                    pintar(1, 4, 7);
+                } else if (cuadro7 == cuadro8 && cuadro7 == cuadro9) {
+                    alert("GANADOR ---> " + cuadro7);
+                    pintar(7, 8, 9);
+                } else if (cuadro7 == cuadro5 && cuadro7 == cuadro3) {
+                    alert("GANADOR ---> " + cuadro7);
+                    pintar(3, 5, 7);
+                }
+                break;
+
+            case 8:
+                if (cuadro8 == cuadro2 && cuadro8 == cuadro5) {
+                    alert("GANADOR ---> " + cuadro8);
+                    pintar(2, 5, 8);
+                } else if (cuadro8 == cuadro7 && cuadro8 == cuadro9) {
+                    alert("GANADOR ---> " + cuadro8);
+                    pintar(7, 8, 9);
+                }
+                break;
+
+            case 9:
+                if (cuadro9 == cuadro3 && cuadro9 == cuadro6) {
+                    alert("GANADOR ---> " + cuadro9);
+                    pintar(3, 6, 9);
+                } else if (cuadro9 == cuadro5 && cuadro9 == cuadro1) {
+                    alert("GANADOR ---> " + cuadro9);
+                    pintar(1, 5, 9);
+                } else if (cuadro9 == cuadro7 && cuadro9 == cuadro8) {
+                    alert("GANADOR ---> " + cuadro9);
+                    pintar(7, 8, 9);
+                }
+                break;
+
+
         }
     }
+}
+
+function pintar(p1,p2,p3){
+    document.getElementById("cuadro" +p1).style.backgroundColor = "Thistle";
+    document.getElementById("cuadro" +p2).style.backgroundColor = "Thistle";
+    document.getElementById("cuadro" +p3).style.backgroundColor = "Thistle";
 }
