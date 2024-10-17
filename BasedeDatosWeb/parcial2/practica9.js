27acfc03b558d4c59ac9635bbbad31b7ab42c820
Millán Sánchez Nancy Paola <159413815+Millansancheznancypaola@users.noreply.github.com>
2024-10-16 18:31:30 -0700

    practica #9 16/10/2024

--- BasedeDatosWeb/parcial2/practica9.js
+++ BasedeDatosWeb/parcial2/practica9.js
@@ -20,4 +20,28 @@ $(document).ready(function(){
             }
         }
     });
+
+    cargarCartas();
+
+
+    function cargarCartas(){
+        var carta1 =0;
+        var carta2 =0;
+        let asignadas = new Array(54).fill(false);
+
+
+        while(carta1 < 16){
+            var num = Math.floor(Math.random()*54) + 1;
+            if(asignadas[num-1] == false){
+                $("#carta1").append("<div class='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+                asignadas[num-1] = true;              
+                carta ++;
+
+
+            } else {
+                continue;
+            }
+
+        }
+    }
 });
